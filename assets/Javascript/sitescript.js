d8809bb64ce17d483729521026b2e590fbb6bbc1
Claudius <mbembac@gmail.com>
2014-03-04 16:31:24 -0500

    edited core files

--- assets/Javascript/sitescript.js
+++ assets/Javascript/sitescript.js
@@ -1,25 +1,22 @@
-
-
 	var setRes;
 	var link = $('a#hlink');
-	var leftPos =0;
+	var leftPos = 0;
 
-	function start(){
+	function start() {
 		link.style.position = "absolute";
 		link.style.left ="0px";
-		link.style.top = "250px";
+		link.style.top = "50px";
 
 		setRes = setInterval(move, 50);
 
 	}
 
-	function move(){
-		leftPos += 5;
-		link.style.left = leftPos + "px";
+	function move() {
+		leftPos+=2;
+		link.style.left=leftPos+"px";
 
-		if(leftPos>1000){
+		if(leftPos>1000) {
 			clearInterval(setRes);
-
 			link.style.position="";
 			link.style.left="";
 			link.style.top="";
