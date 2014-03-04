c801498380b781fef96a008ac2c84186be357f07
Claudius <mbembac@gmail.com>
2014-03-04 17:05:31 -0500

    edited sitescript to have my files

--- assets/Javascript/sitescript.js
+++ assets/Javascript/sitescript.js
@@ -1,60 +1,60 @@
-// 	var setRes;
-// 	var link = document.getElementById("hlink");
-// 	var leftPos = 0;
+var setRes;
+var leftPos = 0;
+var link = $("a#hlink");
 
-// 	function start() {
-// 		link.style.position = "absolute";
-// 		link.style.left ="0px";
-// 		link.style.top = "50px";
 
-// 		setRes = setInterval(move, 50);
-// 	}
-
-// 	function move() {
-// 		leftPos+=2;
-// 		link.style.left=leftPos+"px";
-
-// 		if(leftPos>1000) {
-// 			clearInterval(setRes);
-// 			link.style.position="";
-// 			link.style.left="";
-// 			link.style.top="";
-// 		}
-
-// 	}
-
-// windown.onload = function(){
-// 	setTimeout(start, 5000);	
-// }
-
-var leftPosition = 0;
-var intervalResult;
-var elm = document.getElementById("hlink");
-
-function startAnimate(){
-	elm.style.position = "absolute";
-	elm.style.left = "0px";
-	elm.style.top = "100px";
-	
-	intervalResult = setInterval(animate,50);
+function start() {
+	link.style.position = "absolute";
+	link.style.left ="0px";	
+	link.style.top = "50px";
+	setRes = setInterval(move, 50);
 }
 
-function animate(){
-	leftPosition+=5;
-	elm.style.left = leftPosition +"px";
+function move() {
+	leftPos+=2;
+	link.style.left=leftPos+"px";
 
-	if(leftPosition>900){
-
-		clearInterval(intervalResult);
-		
-		elm.style.position="";
-		elm.style.left="";
-		elm.style.top="";
+	if(leftPos>1000) {
+		clearInterval(setRes);
+		link.style.position="";
+		link.style.left="";
+		link.style.top="";
 	}
 
 }
 
+$("document").ready(function(){
+	setTimeout(start, 5000);	
+});
 
-window.onload = function(){
-	setTimeout(startAnimate, 5000); //call function 5 sec after page is loaded
-}
+// var leftPosition = 0;
+// var intervalResult;
+// var elm = document.getElementById("hlink");
+
+// function startAnimate(){
+// 	elm.style.position = "absolute";
+// 	elm.style.left = "0px";
+// 	elm.style.top = "100px";
+	
+// 	intervalResult = setInterval(animate,50);
+// }
+
+// function animate(){
+// 	leftPosition+=5;
+// 	elm.style.left = leftPosition +"px";
+
+// 	if(leftPosition>900){
+
+// 		clearInterval(intervalResult);
+		
+// 		elm.style.position="";
+// 		elm.style.left="";
+// 		elm.style.top="";
+// 	}
+
+// }
+
+
+// window.onload = function(){
+// 	setTimeout(startAnimate, 2000); //call function 5 sec after page is loaded
+// }
